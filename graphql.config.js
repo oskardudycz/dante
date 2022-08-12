e89efb34225313757ade40603c4ce409cdc0e3f7
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-08-12 15:16:33 +0200

    Added initial gatsby config

---
+++ graphql.config.js
@@ -0,0 +1,4 @@
+// This file is needed to be able to use with VSCode plugin. See:
+// - https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/#configuring-the-vscode-graphql-plugin
+// - https://marketplace.visualstudio.com/items?itemName=GraphQL.vscode-graphql
+module.exports = require("./.cache/typegen/graphql.config.json")
