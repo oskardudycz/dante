87db8ebecc274777b1a1cd3fe122e031d2a46627
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-08-13 19:38:12 +0200

    Added Netlify functions integration to load products list

--- functions/get-products.ts
+++ functions/get-products.ts
@@ -1,9 +1,18 @@
 import { Handler } from '@netlify/functions';
+import products from './data/products.json';
+
+const headers = {
+  /* Required for CORS support to work */
+  'Access-Control-Allow-Origin': 'http://localhost:8000',
+  /* Required for cookies, authorization headers with HTTPS */
+  'Access-Control-Allow-Credentials': true,
+};
 
 const handler: Handler = () => {
   return Promise.resolve({
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    headers,
+    body: JSON.stringify(products),
   });
 };
 
