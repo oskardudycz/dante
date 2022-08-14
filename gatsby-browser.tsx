677f0f5fe79dd2c28a090c23b75e1267df689865
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-08-14 09:44:42 +0200

    Fixed URL choice depending on the environment

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,1 +1,9 @@
+import axios from 'axios';
 import './src/styles/global.css';
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.API_URL
+    : 'http://localhost:8888';
+
+console.log(axios.defaults.baseURL);
